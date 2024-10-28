925588782b52eacad9001b01b28c3ff87bcad9c3
IAKalash <186306458+IAKalash@users.noreply.github.com>
2024-10-29 00:23:34 +0700

    Create Block1_Task4.c

---
+++ myproj/Block1/Block1_Task4.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#pragma warning (disable:4996)
+
+int main(void)
+{
+    FILE* in = fopen("input.txt", "r");
+    FILE* out = fopen("output.txt","w");
+
+    int count,num;
+
+    fscanf(in, "%d", &num);
+    if (num == 1) {
+        fprintf(out, "NO");
+        return 0;
+    }
+    for (count = 2; count < num; count++) {
+        if (num % count == 0) {
+            fprintf(out, "NO");
+            return 1;
+        }
+    }
+    fprintf(out, "YES");
+    
+
+    fclose(in);
+    fclose(out);
+}
