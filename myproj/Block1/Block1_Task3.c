0e8a005622d1354eb6627100d7897ff2116d6ded
IAKalash <186306458+IAKalash@users.noreply.github.com>
2024-10-29 00:23:47 +0700

    Create Block1_Task3.c

---
+++ myproj/Block1/Block1_Task3.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+    FILE* in = fopen("input.txt", "r");
+    FILE* out = fopen("output.txt","w");
+
+    int count = 0, len, sum = 0, num;
+
+    fscanf(in, "%d", &len);
+    while (count++ < len)
+    {
+        fscanf(in, "%d", &num);
+        if (num % 2 == 0)
+            sum += num;
+    }
+    fprintf(out, "%d", sum);
+    
+    fclose(in);
+    fclose(out);
+}
