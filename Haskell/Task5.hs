a40449acc5931fca368531e95fb9beb1078ae869
IAKalash <186306458+IAKalash@users.noreply.github.com>
2024-10-30 00:05:28 +0700

    Create Task5.hs

---
+++ Haskell/Task5.hs
@@ -0,0 +1,28 @@
+or' :: [Bool] -> Bool
+or' = foldr1 (||)
+
+all' :: (a -> Bool) -> [a] -> Bool
+all' f xs = foldr1 (&&) (map f xs) 
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\a b -> f a : b) []
+
+concatMap' :: (a -> [b]) -> [a] -> [b]
+concatMap' f = foldr (\a b -> f a ++ b) []
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' f = foldr g []
+                  where g a b | f a = a : b
+                              | otherwise = b
+
+partition' :: (a -> Bool) -> [a] -> ([a],[a])
+partition' f xs = (filter' f xs , foldr g [] xs)
+                         where g a b | f a = b
+                                     | otherwise = a : b
+
+unzip' :: [(a,b)] -> ([a],[b])
+unzip' = foldr f ([],[])
+                  where f (a,b) (c,d) = (a : c , b : d)
+
+null' :: [a] -> Bool
+null' xs =  
