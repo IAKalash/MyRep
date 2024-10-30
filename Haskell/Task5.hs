e21b8a94f77b3802b4c53e722c74f0ced4a0e217
IAKalash <186306458+IAKalash@users.noreply.github.com>
2024-10-31 00:01:16 +0700

    Update Task5.hs

--- Haskell/Task5.hs
+++ Haskell/Task5.hs
@@ -1,8 +1,9 @@
+
 or' :: [Bool] -> Bool
 or' = foldr1 (||)
 
 all' :: (a -> Bool) -> [a] -> Bool
-all' f xs = foldr1 (&&) (map f xs) 
+all' f xs = foldr1 (&&) (map f xs)
 
 map' :: (a -> b) -> [a] -> [b]
 map' f = foldr (\a b -> f a : b) []
@@ -29,3 +30,23 @@ null' = foldr (\a b -> False) True
 
 intersperse' :: a -> [a] -> [a]
 intersperse' a xs = foldr (\x y -> x : a : y) [] (init xs) ++ [last xs]
+
+group' :: Eq a => [a] -> [[a]]                 -- (Не нашёл способа решить через свёртку)
+group' [] = []
+group' (x : y : xs) | x == y = [x,y] : group' xs
+                    | otherwise = [x] : group' (y:xs)
+
+
+
+lagrange :: [(Double, Double)] -> (Double -> Double)
+lagrange points = \x -> foldl (\acc (x_i,y_i) -> acc + (y_i * l x_i x)) 0 points
+    where l x_i x = foldl (\acc (x_j, _) -> if x_i == x_j then acc else acc * (x - x_j) / (x_i - x_j)) 1 points
+
+
+
+-- 1. Да, одинаковый: [Int] -> Int (или другой числовой формат вместо Int)
+-- 2. xs = [1] (подойдёт практически любой непустой список)
+-- 3. [0] (список из 0 любой длины)
+-- 4. f [1,2,3] <=> a - (b - (c - 0))       g [1,2,3] <=> 0 - a - b - c
+--  Откуда a = -c. Значит, троек подходящих под условие бесконечно много 
+-- (под данное условие подходят все тройки, где a = -c)
