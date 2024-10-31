a7a0cb0ae2950916b6ef4f3dd204b8c819833fe3
IAKalash <186306458+IAKalash@users.noreply.github.com>
2024-10-31 10:08:13 +0700

    Update Task4.hs

--- Haskell/Task4.hs
+++ Haskell/Task4.hs
@@ -1,17 +1,3 @@
-import Data.List
-
-len' :: [a] -> Int
-len' [] = 0
-len' (x : xs) = 1 + len' xs
-
-map' :: ([a] -> [b]) -> [[a]] -> [[b]]
-map' f (x : xs) = f x : map' f xs
-
-map2 :: (a -> c) -> [a] -> [a] -> [c]
-map2 f [] ys = []
-map2 f xs [] = []
-map2 f (x : xs) (y : ys) = f x : f y : map2 f xs ys
-
 triples:: [(Int,Int,Int)]
 triples = [(x,y,z) | z <- [1..], y <- [1..z] , x <- [1..y], x^2 + y^2 == z^2]
 
@@ -19,8 +5,15 @@ collatz :: Int -> [Int]
 collatz n | n <= 0 = error "incorrect input"
           | n == 1 = [n]
           | even n = n : collatz (div n 2)
-          | odd n = n : collatz (n * 3 + 1) 
+          | odd n = n : collatz (n * 3 + 1)
 
-permutations' :: (Eq a) => [a] -> [[a]]
-permutations' [] = [[]]
-permutations' xs = [a:x | a <- xs, x <- (permutations' (filter (\x -> x /= a) xs))]
+permutations :: Eq a => [a] -> [[a]]
+permutations [] = [[]]
+permutations (x:xs) = [y:ys | y <- x:xs, ys <- permutations (f y (x:xs))]
+                                                            where f :: Eq a => a -> [a] -> [a]
+                                                                  f n (x:xs) | n == x = xs
+                                                                             | otherwise = x: (f n xs) 
+
+subsequenses :: [a] -> [[a]]
+subsequenses [] = [[]]
+subsequenses (x:xs) = map (x:) (subsequenses xs) ++ subsequenses xs
