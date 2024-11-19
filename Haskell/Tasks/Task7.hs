5e9476728f4d5afa116a12494cf2792d9351d4ed
IAKalash <186306458+IAKalash@users.noreply.github.com>
2024-11-20 00:11:29 +0700

    T7

--- Haskell/Tasks/Task7.hs
+++ Haskell/Tasks/Task7.hs
@@ -35,7 +35,23 @@ boolEval (Bigger ex1 ex2) = eval ex1 > eval ex2
 
 
 data Bin = End | O Bin | I Bin
-    deriving (Show, Eq)
+    deriving Show
+
+instance Eq Bin where
+
+  End == End = True
+  End == ex = False
+  ex == End = False
+  
+  (I ex1) == (O ex2) = False
+  (O ex1) == (I ex2) = False
+  (I ex1) == (I ex2) = ex1 == ex2
+  (O ex1) == (O ex2) = ex1 == ex2
+
+  -- (ex1 (O End)) == ex2 = (ex1 End) == ex2
+  -- ex1 == (ex2 (O End)) = ex1 == (ex2 End)
+
+  
 
 inc :: Bin -> Bin
 inc End = I End
@@ -55,7 +71,37 @@ toBin n | mod n 2 == 1 = I (toBin (div n 2))
         | otherwise = O (toBin (div n 2))
 
 pls :: Bin -> Bin -> Bin
-pls = undefined
+
+pls (End) ex = ex
+pls ex (End) = ex
+
+pls (O End) (O End) = O End
+pls (O ex1) (O End) = O ex1
+pls (O End) (O ex2) = O ex2
+
+pls (I End) (I End) = O (I End)
+pls (I ex1) (I End) = O (inc ex1)
+pls (I End) (I ex2) = O (inc ex2)
+
+pls (I End) (O ex2) = I ex2
+pls (O ex1) (I End) = I ex1
+
+pls (O ex1) (O ex2) = O (pls ex1 ex2)
+pls (I ex1) (O ex2) = I (pls ex1 ex2)
+pls (O ex1) (I ex2) = I (pls ex1 ex2)
+pls (I ex1) (I ex2) = O (pls (pls ex1 ex2) (I End))
+
+
 
 mlt :: Bin -> Bin -> Bin
-mlt = undefined
+
+mlt (O End) _ = O End
+mlt _ (O End) = O End
+
+mlt (I End) ex = ex
+mlt ex (I End) = ex
+
+mlt (ex1) (O ex2) = O (mlt ex1 ex2)
+mlt (ex1) (I ex2) = pls ex1 (O (mlt ex1 ex2))
+
+
