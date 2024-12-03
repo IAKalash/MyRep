6bee9a01114000c9db0fc787457f1063fca5cc2d
IAKalash <186306458+IAKalash@users.noreply.github.com>
2024-12-04 00:38:57 +0700

    Hs upd

--- Haskell/Tasks/Task8.hs
+++ Haskell/Tasks/Task8.hs
@@ -2,13 +2,33 @@ data Tree a = Empty | Node a (Tree a) (Tree a)
     deriving (Read, Eq)
 
 instance Show a => Show (Tree a) where
-    show Empty = ""
-    show (Node n Empty Empty) = show n
-    show (Node n a b) = show a ++ "  /  " ++ show n ++ "  \\  " ++ show b
+    
+
+
 
 isSym :: Eq a => Tree a -> Bool
 isSym Empty = error "Empty Tree"
 isSym (Node n l r) = isSym' l r
+
+buildTree :: Ord a => [a] -> Tree a
+buildTree [] = error "Empty list"
+buildTree xs = buildTree' xs Empty
+
+deleteMin :: (Ord a) => Tree a -> (Tree a, a)
+deleteMin Empty = error "Empty tree"
+deleteMin a = (deleteValue (minEl a) a, minEl a)
+
+deleteValue :: (Ord a) => a -> Tree a -> Tree a
+deleteValue x Empty = Empty
+deleteValue x (Node n Empty Empty) | x == n = Empty
+deleteValue x (Node n l Empty) | x == n = l
+deleteValue x (Node n Empty r) | x == n = r
+deleteValue x (Node n l r) | x < n = Node n (deleteValue x l) r
+                           | x > n = Node n l (deleteValue x r)
+                           | x == n = insertTree r l
+
+
+
 
 isSym' :: Eq a => Tree a -> Tree a -> Bool
 isSym' Empty Empty = True
@@ -16,10 +36,6 @@ isSym' Empty Node {} = False
 isSym' Node {} Empty = False
 isSym' (Node n l r) (Node n2 l2 r2) | n == n2 = isSym' l r2 && isSym' r l2
                                     | otherwise = False
-
-buildTree :: Ord a => [a] -> Tree a
-buildTree [] = error "Empty list"
-buildTree xs = buildTree' xs Empty
 
 buildTree' :: Ord a => [a] -> Tree a -> Tree a
 buildTree' [] n = n
@@ -31,20 +47,6 @@ insert x Empty = Node x Empty Empty
 insert x (Node n l r) | x < n = Node n (insert x l) r
                       | x > n = Node n l (insert x r)
 
-deleteMin :: (Ord a) => Tree a -> (Tree a, a)
-deleteMin Empty = error "Empty tree"
-deleteMin a = ((deleteValue (minEl a) a), minEl a)
-
-
-deleteValue :: (Ord a) => a -> Tree a -> Tree a
-deleteValue x Empty = Empty
-deleteValue x (Node n Empty Empty) | x == n = Empty
-deleteValue x (Node n l Empty) | x == n = l
-deleteValue x (Node n Empty r) | x == n = r
-deleteValue x (Node n l r) | x < n = Node n (deleteValue x l) r
-                           | x > n = Node n l (deleteValue x r)
-                           | x == n = insertTree r l
-
 insertTree :: Ord a => Tree a -> Tree a -> Tree a
 insertTree Empty b = b
 insertTree a Empty = a
@@ -55,3 +57,13 @@ insertTree (Node n l r) (Node n2 l2 r2) | n2 == n = Node n (insertTree l2 l) (in
 minEl :: Tree a -> a
 minEl (Node n Empty _) = n
 minEl (Node n l _) = minEl l
+
+
+-- show Empty = ""
+    -- show (Node n Empty Empty) = show n
+    -- show (Node n (Node a l r) Empty) = "    " ++ show n ++ "\n" ++ "   /\n  " ++ show a ++ "     " ++ "\n  /  \\" 
+    --         ++ "\n " ++ show l ++ "   " ++ show r
+    -- show (Node n Empty (Node a l r)) = "    " ++ show n ++ "\n" ++ "  \\  \n  " ++ show a ++ "     " ++ "\n    /  \\" 
+    --         ++ "\n " ++ show l ++ "   " ++ show r
+    -- show (Node n (Node nl al bl) (Node nr ar br)) = "    " ++ show n ++ "\n" ++ "   /  \\ \n  " ++ show nl ++ "     " ++ show nr 
+    --                 ++ "\n  /  \\   /  \\" ++ "\n " ++ show al ++ "   " ++ show bl ++ "  " ++ show ar ++ "   " ++ show br
