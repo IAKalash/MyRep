5d5e7d50a0434bfd9346cf134faaa62d91717787
IAKalash <186306458+IAKalash@users.noreply.github.com>
2024-12-05 09:41:18 +0700

    upd

--- myproj/Block11_Task3.c
+++ myproj/Block11_Task3.c
@@ -14,18 +14,20 @@ int main(void)
 
     int a, b, c, M;
     uint64_t state = 1;
-    uint64_t hash[1000];
+    uint64_t hash[100000];
 
     scanf("%d", &M);
     scanf("%d %d %d", &a, &b, &c);
 
-    for (int i = 0; i < 50; ++i) {
-        TEST[i] = state;
-        state = func(state, a, b, c, M);
-    }
-
-    for (int i = 0; i < 50; ++i) {
-        printf("%d ", TEST[i]);
+    for (int i = 0; i != -1; ++i) {
+        if (hash[state % 100000] == 0) {
+            hash[state % 100000] = i;
+            state = func(state, a, b, c, M);
+        }
+        else {
+            printf("%d %d", hash[state % 100000], i);
+            break;
+        }
     }
 
     fclose(stdin);
