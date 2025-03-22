48a138e8d80e1b8eef20c5b4a6a7c08c5d6087a9
IAKalash <152734199I@mail.ru>
2025-03-22 17:28:04 +0700

    Wolfs'nSheep

--- Haskell/2ndsem/Tasks/WolfsAndSheep.hs
+++ Haskell/2ndsem/Tasks/WolfsAndSheep.hs
@@ -1,8 +1,12 @@
 module WolfsAndSheep where
 
+import Control.Monad.Trans
 import Control.Monad.Trans.Except
 import Control.Monad.Trans.Writer
 import Data.Char
+import System.Random
+import System.IO.Unsafe ( unsafePerformIO )
+
 
 -- Положение на доске
 data Position = Position {f :: Char, r :: Int}
@@ -37,7 +41,7 @@ printGame (Game sheep wolfs) =  "\n8 " ++ checkLine sheep wolfs 8 line  ++
                                 "\n1 " ++ checkLine sheep wolfs 1 line1
 
 checkLine :: Position -> [Position] -> Int -> String -> String
-checkLine sheep wolfs sym str | num sheep == sym = replace str 'S' (ord (head (show sheep)) - 97)
+checkLine sheep wolfs sym str | num sheep == sym = replace (checkLine (Position 'a' 10) wolfs sym str) 'S' (ord (head (show sheep)) - 97)
                               | any ((==sym) . num) wolfs = replWolfs (map ((\x -> x - 97) . ord . char) (isInLine sym wolfs)) str
                               | otherwise = str
 
@@ -89,8 +93,8 @@ choose sym (x:y:xs) | sym == x = [y]
 
 
 possibleWolfsSteps :: Game -> [Step] -- list comprehension
-possibleWolfsSteps (Game sheep [Position ch1 n1, Position ch2 n2, Position ch3 n3, Position ch4 n4]) = isCollision (unique
-        [Step (Position char num) (Position char2 (ord num2 - 48)) | char <- ch1 : ch2 : ch3 : [ch4], num <- n1 : n2 : n3 : [n4], char2 <- choose char "abcdefgh", num2 <- chooseW (chr (num + 48)) "87654321"]) (sheep : [Position ch1 n1, Position ch2 n2, Position ch3 n3, Position ch4 n4])
+possibleWolfsSteps (Game sheep wolfs) = isCollision (unique [Step pos (Position char2 (ord num2 - 48))
+        | pos <- wolfs , char2 <- choose (char pos) "abcdefgh", num2 <- chooseW (chr (num pos + 48)) "87654321"]) (sheep : wolfs)
 
 isCollision :: [Step] -> [Position] -> [Step]
 isCollision [] ws = []
@@ -116,21 +120,21 @@ possibleSheepSteps2 (Game (Position char num) wolfs) = isCollision (unique f) (P
                 return (Step (Position char num) (Position ch (ord n - 48)))
 
 possibleWolfsSteps2 :: Game -> [Step] -- do-notation
-possibleWolfsSteps2 (Game sheep [Position ch1 n1, Position ch2 n2, Position ch3 n3, Position ch4 n4]) = isCollision (unique f) (sheep : [Position ch1 n1, Position ch2 n2, Position ch3 n3, Position ch4 n4])
+possibleWolfsSteps2 (Game sheep wolfs) = isCollision (unique f) (sheep : wolfs)
         where f = do
-                char <- ch1 : ch2 : ch3 : [ch4]
-                num <- n1 : n2 : n3 : [n4]
-                ch <- choose char "abcdefgh"
-                n <- chooseW (chr (num + 48)) "87654321"
-                return (Step (Position char num) (Position ch (ord n - 48)))
+                pos <- wolfs
+                ch <- choose (char pos) "abcdefgh"
+                n <- chooseW (chr (num pos + 48)) "87654321"
+                return (Step pos (Position ch (ord n - 48)))
 
 
 simpleTurn :: Game -> Either GameResult Game
-simpleTurn (Game (Position ch n) wolfs) | n == 8 = Left SheepWin
-                                        | null (possibleSheepSteps (Game (Position ch n) wolfs)) = Left WolfsWin
-                                        | otherwise =
-                                                return (Game (sheepStep $ head $ possibleSheepSteps (Game (Position ch n) wolfs))
-                                                (wolfsStep wolfs (head $ possibleWolfsSteps (Game (Position ch n) wolfs))))
+simpleTurn (Game sheep wolfs) | num sheep == 8 = Left SheepWin
+                              | null (possibleSheepSteps (Game sheep wolfs)) = Left WolfsWin
+                              | null (possibleWolfsSteps (Game sheep wolfs)) = Left SheepWin
+                              | otherwise = do
+                                let shPos = sheepStep $ head $ possibleSheepSteps (Game sheep wolfs)
+                                return (Game shPos (wolfsStep wolfs (head $ possibleWolfsSteps (Game shPos wolfs))))
 
 
 sheepStep :: Step -> Position
@@ -142,9 +146,94 @@ wolfsStep (x:xs) (Step oldpos newpos) | oldpos == x = newpos : xs
                                       | otherwise = x : wolfsStep xs (Step oldpos newpos)
 
 run :: Monad m => (a -> m a) -> a -> m a
-run turn = 
+run turn game = turn game >>= run turn
 
 loggedTurn :: Game -> (WriterT [Step] (Either GameResult)) Game
-loggedTurn = undefined
+loggedTurn (Game sheep wolfs) | num sheep == 8 = lift $ Left SheepWin
+                              | null (possibleSheepSteps (Game sheep wolfs)) = lift $ Left WolfsWin
+                              | null (possibleWolfsSteps (Game sheep wolfs)) = lift $ Left SheepWin
+                              | otherwise = do
+                                let shStep = head $ possibleSheepSteps (Game sheep wolfs)
+                                let wlStep = head $ possibleWolfsSteps (Game (sheepStep shStep) wolfs)
+                                tell $ shStep : [wlStep]
+                                return (Game (sheepStep shStep) (wolfsStep wolfs wlStep))
+
 loggedTurn' :: Game -> ExceptT GameResult (Writer [Step]) Game
-loggedTurn' = undefined
+loggedTurn' (Game sheep wolfs) | num sheep == 8 = ExceptT $ return $ Left SheepWin
+                               | null (possibleSheepSteps (Game sheep wolfs)) = ExceptT $ return $ Left WolfsWin
+                               | null (possibleWolfsSteps (Game sheep wolfs)) = ExceptT $ return $ Left SheepWin
+                               | otherwise = do
+                                let shStep = head $ possibleSheepSteps (Game sheep wolfs)
+                                let wlStep = head $ possibleWolfsSteps (Game (sheepStep shStep) wolfs)
+                                lift $ tell $ shStep : [wlStep]
+                                return (Game (sheepStep shStep) (wolfsStep wolfs wlStep))
+
+-- Функции отличаются выводом из-за разной конфигурации монад. Первая выводит только результат при завершении игры, 
+-- а вторая выводит результат и лог (при промежуточных шагах обе функции выводят лог помимо промежуточного результата)
+-- Предпочтительнее использовать вторую функцию чтобы не терять логи
+
+randomTurn :: Game -> ExceptT GameResult (Writer [Step]) Game
+randomTurn (Game sheep wolfs) | num sheep == 8 = ExceptT $ return $ Left SheepWin
+                              | null (possibleSheepSteps (Game sheep wolfs)) = ExceptT $ return $ Left WolfsWin
+                              | null (possibleWolfsSteps (Game sheep wolfs)) = ExceptT $ return $ Left SheepWin
+                              | otherwise = do
+                                let shStep = rnd $ possibleSheepSteps (Game sheep wolfs)
+                                let step = possibleWolfsSteps (Game (sheepStep shStep) wolfs)
+                                case step of
+                                        [] -> ExceptT $ return $ Left SheepWin
+                                        _ -> do
+                                                let wlStep = rnd step
+                                                lift $ tell [shStep,wlStep]
+                                                return (Game (sheepStep shStep) (wolfsStep wolfs wlStep))
+
+rnd :: [Step] -> Step
+rnd steps = unsafePerformIO $ do
+    randNum <- randomRIO (0, len steps - 1)
+    return $ steps !! randNum
+
+len :: [a] -> Int
+len = foldr (\ x -> (+) 1) 0
+
+runGame :: Game -> IO [Step] -- is needed only for playerTurn (Task 6)
+runGame game = do
+        (res, log) <- playerTurn game
+        case res of 
+                Left SheepWin -> do
+                        print res
+                        putStrLn "Log:"
+                        return log
+                Left WolfsWin -> do
+                        print res
+                        putStrLn "Log:"
+                        return log
+                Right next -> do
+                        rest <- runGame next
+                        return (log ++ rest)
+
+--PlayerTurn не работает с оператором >>= и run из-за наличия в сигнатуре "грязной" монады IO, которую невозможно "развернуть"
+
+playerTurn :: Game -> IO (Either GameResult Game, [Step])
+playerTurn game = runWriterT (runExceptT (playerTurn' game))
+
+playerTurn' :: Game -> ExceptT GameResult (WriterT [Step] IO) Game 
+playerTurn' (Game sheep wolfs) | num sheep == 8 = ExceptT $ return $ Left SheepWin
+                              | null (possibleSheepSteps (Game sheep wolfs)) = ExceptT $ return $ Left WolfsWin
+                              | null (possibleWolfsSteps (Game sheep wolfs)) = ExceptT $ return $ Left SheepWin
+                              | otherwise = do
+                                liftIO $ print $ Game sheep wolfs
+                                liftIO $ putStrLn ("Possible turns: " ++ show (possibleSheepSteps (Game sheep wolfs)))
+                                liftIO $ putStrLn "Print the new position of the sheep like in this way: b2"
+                                line <- liftIO getLine
+                                let choise = Step sheep (Position (head line) ((\x -> x - 48) $ ord $ last line))
+                                if choise `elem` possibleSheepSteps (Game sheep wolfs)
+                                        then do 
+                                                let step = possibleWolfsSteps (Game (sheepStep choise) wolfs)
+                                                case step of
+                                                        [] -> ExceptT $ return $ Left SheepWin
+                                                        _ -> do
+                                                                let wlStep = rnd step
+                                                                lift $ tell [choise,wlStep]
+                                                                return (Game (sheepStep choise) (wolfsStep wolfs wlStep))
+                                        else do
+                                                liftIO $ putStrLn "\nIncorrect step"
+                                                playerTurn' (Game sheep wolfs)
